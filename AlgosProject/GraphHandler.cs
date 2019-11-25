8b0dd966f91332f829dddbb257fc859a3e19a23a
jgalyardt <jgalyardt@gmail.com>
2019-11-25 14:56:26 -0600

    started part 2

---
+++ AlgosProject/GraphHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AlgosProject
+{
+    class GraphHandler
+    {
+        string pathToP;
+        string pathToE;
+        bool graphBuilt = false;
+
+        public GraphHandler(string pPath, string ePath)
+        {
+            pathToP = pPath;
+            pathToE = ePath;
+        }
+
+        public void BuildGraph()
+        {
+            StreamReader srP = new StreamReader(pathToP);
+            String line = srP.ReadLine();
+            while (line != null)
+            {
+                try
+                {
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"Unable to parse '{line}'");
+                }
+
+                line = srP.ReadLine();
+            }
+        }
+
+        public void SmallestLast()
+        {
+            if (!graphBuilt)
+            {
+                BuildGraph();
+            }
+
+        }
+    }
+}
